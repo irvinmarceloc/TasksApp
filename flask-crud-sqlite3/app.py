9cb32b9e3bbfeb3106a0e67bc56cb9184c59370b
irvinmarceloc <irvinmarceloc@gmail.com>
2021-03-18 18:34:04 -0300

    Base Completa

--- flask-crud-sqlite3/app.py
+++ flask-crud-sqlite3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,12 +13,28 @@ class Task(db.Model):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks)
 
 @app.route('/create-task', methods=['POST'])
 def create():
-    Task(content = request.form['content'], done=False) 
-    #db.session.add()
+    task = Task(content = request.form['content'], done=False) 
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
