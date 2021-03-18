d7f994cbb4499abd2e87323cbea0df3c701aca08
irvinmarceloc <irvinmarceloc@gmail.com>
2021-03-18 17:39:02 -0300

    Base1

--- flask-crud-sqlite3/app.py
+++ flask-crud-sqlite3/app.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/create-task', methods=['POST'])
+def create():
+    Task(content = request.form['content'], done=False) 
+    #db.session.add()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
